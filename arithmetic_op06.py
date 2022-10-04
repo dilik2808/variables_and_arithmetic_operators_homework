8ff8a39e8b61c4c70652b9c2a89f01183866840b
dilik2808 <dilik2808@gmail.com>
2022-10-04 22:44:24 +0500

    oltinchisi

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,12 @@
 # Create a variable "a"  and assign it the value of the int 7.
+from re import A
 
+
+a=7
 # Create a variable "b"  and assign it the value of the int 3.
-
+b=3
 # Create a variable "c"  and assign it the value of the int 5.
-
+c=5
 # Create a variable called "answer".
-
+answer=a+b+c
 # Find the sum of a, b and c then assign to "answer".
